b11aa3e7828b9bba3755e3391cd3c383cc792c1f
Khinshan Khan <khanshan501@gmail.com>
2020-08-04 22:25:33 -0400

    update netlify settings to be more transparent

--- config/plugins.js
+++ config/plugins.js
@@ -1,6 +1,14 @@
 module.exports = [
   "gatsby-plugin-root-import",
   {
+    resolve: "gatsby-plugin-material-ui",
+    options: {
+      stylesProvider: {
+        injectFirst: true,
+      },
+    },
+  },
+  {
     resolve: "gatsby-plugin-prefetch-google-fonts",
     options: {
       fonts: [
