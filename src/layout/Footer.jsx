eb7d45637c8824eff60ee676c5fcda0a1be6e360
Max Zlotskiy <max.zlotskiy@gmail.com>
2020-08-31 23:15:03 -0400

    added more content to footer, closes #9

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -1,7 +1,80 @@
 import React from "react";
+import { Link as GatsbyLink } from "gatsby";
+import Typography from "@material-ui/core/Typography";
+import Link from "@material-ui/core/Link";
+import { Facebook, Twitter, RssFeed } from "@material-ui/icons";
+import Grid from "@material-ui/core/Grid";
 
 const Footer = ({ title }) => {
-  return <>&copy; {title} 2020</>;
+  return (
+    <>
+      <hr />
+      <Typography
+        variant="h5"
+        color="textSecondary"
+        align="center"
+        noWrap
+      >
+        <Link
+          to="/"
+          component={GatsbyLink}
+          color="inherit"
+        >
+          {title}
+        </Link>
+      </Typography>
+      <Grid 
+        container 
+        direction="row"
+        justify="center" 
+        alignItems="center"
+        spacing={2}
+      >
+        <Grid item>
+          <Facebook />
+        </Grid>
+        <Grid item>
+          <Twitter />
+        </Grid>
+        <Grid item>
+          <RssFeed />
+        </Grid>
+      </Grid>
+      <Grid 
+        container 
+        direction="row"
+        justify="center" 
+        alignItems="center"
+        spacing={2}
+      >
+        <Grid item>
+          <Link 
+            to="mailto:max.zlotskiy16@myhunter.cuny.edu"
+          >
+            Contact
+          </Link>
+        </Grid>
+        <Grid item>
+          <Link to="/">
+            About Us
+          </Link>
+        </Grid>
+        <Grid item>
+          <Link to="/">
+            Write For Us
+          </Link>
+        </Grid>
+        <Grid item>
+          <Link to="/">
+            Join the team
+          </Link>
+        </Grid>
+      </Grid>
+      <Typography variant="body1" align="center">
+        &copy; {title} 2020 - {(new Date()).getFullYear()}
+      </Typography>
+    </>
+  );
 };
 
 export default React.memo(Footer);
