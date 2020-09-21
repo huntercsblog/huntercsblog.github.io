149e0d471800babc7d5636cbd736d11e211d80d5
Max Zlotskiy <max.zlotskiy@gmail.com>
2020-09-20 23:27:31 -0400

    article lists authors and time

--- src/templates/article.jsx
+++ src/templates/article.jsx
@@ -1,14 +1,30 @@
 import React, {Fragment} from "react";
-import { Link, graphql } from "gatsby";
+import { Link as GatsbyLink, graphql } from "gatsby";
 import { MDXRenderer } from "gatsby-plugin-mdx";
 import Typography from "@material-ui/core/Typography";
+import Link from "@material-ui/core/Link";
+import Avatar from '@material-ui/core/Avatar';
+import Grid from '@material-ui/core/Grid';
+import { makeStyles } from '@material-ui/core/styles';
 
 import SEO from "src/components/SEO";
 import Layout from "src/layout";
 
+const useStyles = makeStyles((theme) => ({
+  avatarContainer: {
+    display: "flex",
+    "& > *": {
+      marginRight: theme.spacing(1),
+    },
+  },
+  chips: {
+    marginRight: theme.spacing(1),
+  },
+}));
+
 const Article = ({ data, pageContext }) => {
-  const { frontmatter, body } = data.mdx;
-  console.log(pageContext);
+  const { frontmatter, body, timeToRead } = data.mdx;
+  const classes = useStyles();
   return (
     <Layout>
       <SEO 
@@ -16,11 +32,40 @@ const Article = ({ data, pageContext }) => {
         description={body.substring(0,140).replace(/^#.*?$/gm, "")} 
         article={true} 
       />
-      <Typography variant="h1">{frontmatter.title}</Typography>
-      <Typography variant="subtitle1">{frontmatter.date}</Typography>
+      {/*Title*/}
+      <Typography variant="h3">{frontmatter.title}</Typography>
+      {/*Authors*/}
+      <Grid container alignItems="center">
+        {/*Author Pictures*/}
+        <Grid item>
+          <div className={classes.avatarContainer}>
+          {frontmatter.authors.map(a => 
+            <Avatar alt={a.display_name} src={a.image}>{a.username}</Avatar>
+          )}
+          </div>
+        </Grid>
+        {/*Author Names/Links*/}
+        <Grid item>
+          <Typography variant="subtitle1">
+            By {frontmatter.authors.map((a, index, array) => 
+              <Fragment>
+                <Link component={GatsbyLink} to={`/author/${a.username}`}>
+                  {a.display_name}
+                </Link>
+                {index === array.length-2 && " and "}
+                {index < array.length-2 && ", "}
+              </Fragment>
+            )}
+          </Typography>
+        </Grid>
+      </Grid>
+      {/*Date Published*/}
+      <Typography variant="subtitle1" component="time">
+        {" " + frontmatter.date + " "}&middot;{" " + timeToRead + " min read"}
+      </Typography>
       <MDXRenderer>{body}</MDXRenderer>
       {pageContext.prev && (
-        <Link to={`/articles/${pageContext.prev.node.fields.slug}`}>
+        <Link to={`/articles/${pageContext.prev.node.fields.slug}`} component={GatsbyLink}>
           <Fragment>
             {"<--"}
             {pageContext.prev.node.frontmatter.title}
@@ -28,7 +73,7 @@ const Article = ({ data, pageContext }) => {
         </Link>
       )}
       {pageContext.next && (
-        <Link to={`/articles/${pageContext.next.node.fields.slug}`}>
+        <Link to={`/articles/${pageContext.next.node.fields.slug}`} component={GatsbyLink}>
           <Fragment>
             {"-->"}
             {pageContext.next.node.frontmatter.title}
@@ -44,8 +89,14 @@ export const query = graphql`
     mdx(fields: { slug: { eq: $slug } }) {
       frontmatter {
         title
-        date(formatString: "YYYY MMMM Do")
+        date(formatString: "MMMM Do, YYYY")
+        authors {
+          username
+          display_name
+          image
+        }
       }
+      timeToRead
       body
     }
   }
