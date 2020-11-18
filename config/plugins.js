08aab9d0715e8fe8794418c925329e81af1634da
Max Zlotskiy <max.zlotskiy@gmail.com>
2020-11-18 13:49:15 -0500

    replaced google fonts plugin with newer plugin

--- config/plugins.js
+++ config/plugins.js
@@ -10,7 +10,7 @@ module.exports = [
       },
     },
   },
-  {
+  /*{
     resolve: "gatsby-plugin-prefetch-google-fonts",
     options: {
       fonts: [
@@ -23,7 +23,7 @@ module.exports = [
         },
       ],
     },
-  },
+  },*/
   {
     resolve: `gatsby-plugin-mdx`,
     options: {
