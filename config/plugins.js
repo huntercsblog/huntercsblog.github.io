c781da528c270b13d640a7fac4901f62bcb762bf
Max Zlotskiy <max.zlotskiy@gmail.com>
2020-11-22 17:59:06 -0500

    remark plugins can be put under mdx plugin

--- config/plugins.js
+++ config/plugins.js
@@ -28,6 +28,36 @@ module.exports = [
     resolve: `gatsby-plugin-mdx`,
     options: {
       extensions: [`.mdx`, `.md`],
+      gatsbyRemarkPlugins: [
+        {
+          resolve: `gatsby-remark-prismjs`,
+          options: {
+            // This is used to allow setting a language for inline code
+            // (i.e. single backticks) by creating a separator.
+            // This separator is a string and will do no white-space
+            // stripping.
+            // A suggested value for English speakers is the non-ascii
+            // character '›'.
+            inlineCodeMarker: "±",
+            // This lets you set up language aliases.  For example,
+            // setting this to '{ sh: "bash" }' will let you use
+            // the language "sh" which will highlight using the
+            // bash highlighter.
+            aliases: {"c++": "cpp", "js": "javascript"},
+            // This toggles the display of line numbers globally alongside the code.
+            // To use it, add the following line in gatsby-browser.js
+            // right after importing the prism color scheme:
+            //  require("prismjs/plugins/line-numbers/prism-line-numbers.css")
+            // Defaults to false.
+            // If you wish to only show line numbers on certain code blocks,
+            // leave false and use the {numberLines: true} syntax in blocks
+            showLineNumbers: false,
+            // If setting this to true, the parser won't handle and highlight inline
+            // code used in markdown i.e. single backtick code like `this`.
+            noInlineHighlight: false,
+          },
+        },
+      ],
     },
   },
   {
