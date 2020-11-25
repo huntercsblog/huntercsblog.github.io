bb6962ad50fe377af5d5dc4b47db381a238a3ca6
Max Zlotskiy <max.zlotskiy@gmail.com>
2020-11-25 17:26:04 -0500

    added google form

--- src/pages/subscribe.jsx
+++ src/pages/subscribe.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import Typography from "@material-ui/core/Typography";
+import Link from "@material-ui/core/Link";
+import LaunchIcon from '@material-ui/icons/Launch';
 import {Helmet} from "react-helmet";
 
 import SEO from "src/components/SEO";
@@ -17,6 +19,7 @@ const PrivacyPage = () => {
         the form below to receive this newsletter to be notified of our 
         featured articles and tech news. You can unsubscribe or resubscribe at
         any time.
+        <Typography variant="h6">If you want to use your myhunter email, or you didn't get a confirmation email, fill out <Link color="textSecondary" href="https://docs.google.com/forms/d/e/1FAIpQLSdG8G0CqsStJqva8PVIr6v7Bf-VxJY7awffp6ssexUx4E000A/viewform?usp=sf_link"><LaunchIcon />&nbsp;this form instead</Link></Typography>
       </p>
       <Helmet>
         <script type="text/javascript" src="https://campaigns.zoho.com/js/zc.iframe.js"></script>
