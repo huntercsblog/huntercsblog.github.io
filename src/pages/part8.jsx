ec54f72b76aa31e50f0d5a3f6810893332a25680
Alan Hu <hi@Alans-MacBook-Air.local>
2021-07-20 10:26:01 -0400

    fixed link

--- src/pages/part8.jsx
+++ src/pages/part8.jsx
@@ -71,7 +71,7 @@ const Part8 = () => {
       {/*<p>Itaque earum rerum hic tenetur a sapiente delectus. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit.</p>
       <p>Non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. At vero eos et accusamus. Facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam.</p>/*}
       {/* TODO: (4) Add a Link component. Ctrl+f for it in about.jsx to see how to use it.*/}
-      <Link title = "New Article" href = "/content/publications/New-Article">New Article</Link>
+      <Link title = "New Article" href = "/articles/2021/07/14/New-Article">New Article</Link>
       {/* Make it link to your article. The title attribute is the text that appears when you hover over the link. */}
       {/* The Link component boils down to an <a> tag */}
       </div>
