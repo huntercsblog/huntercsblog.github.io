e2ce597473b77259edaa41214c2f4c9e91cc14c1
Alicia <akthyang@gmail.com>
2021-07-29 12:14:39 -0400

    Implemented local search and styled ending results

--- src/pages/results.jsx
+++ src/pages/results.jsx
@@ -1,9 +1,14 @@
-import React, {useState} from "react";
+import React, {Fragment, useState} from "react";
 import Layout from "src/layout";
 import Typography from "@material-ui/core/Typography";
-import "src/assets/styles/results.css";
-// import { useFlexSearch } from 'react-use-flexsearch';
+import Box from "@material-ui/core/Box";
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardContent from '@material-ui/core/CardContent';
+import { useFlexSearch } from 'react-use-flexsearch';
 import { graphql, useStaticQuery } from "gatsby";
+import { Link as GatsbyLink } from "gatsby";
+import "src/assets/styles/results.css";
 
 const Result = props => {
   const pageQuery =useStaticQuery(graphql`
@@ -12,28 +17,11 @@ const Result = props => {
         index
         store
       }
-      allMdx(
-        sort: { fields: frontmatter___date, order: DESC }
-      ) {
-        edges {
-          node {
-            frontmatter {
-              title
-              humanDate: date(formatString: "MMM Do, YYYY")
-              datetime: date(formatString: "YYYY-MM-DD")
-            }
-            fields {
-              slug
-            }
-            excerpt(truncate: true, pruneLength: 300)
-            timeToRead
-            rawBody
-          }
-        }
-      }
     }
   `);
-  const index = pageQuery.index;
+  const {index, store} = pageQuery.localSearchPages;
+  const [searchQuery, setSearchQuery] = useState(' ');
+  const results = useFlexSearch(searchQuery, index, store);
   return (
     <Layout>
       <div id = "Search">
@@ -41,13 +29,33 @@ const Result = props => {
           type = "text"
           placeholder = "Search posts"
           id = "inptxt"
-
+          value = {searchQuery}
+          onInput = {(e) => setSearchQuery(e.target.value)}
         />
           <div class="icons8-search"></div>
       </div>
-      <Typography variant = "h5">
-        Search Results for ""
-      </Typography>
+      <Fragment>
+        <Typography variant="h4">
+          Search Results for "{searchQuery}"
+        </Typography>
+        {results.map(result => (
+            <Box component={Card} my={1} key={result}>
+              <CardActionArea component={GatsbyLink} to={`/articles/${result.slug}`}>
+                <CardContent>
+                  <Typography variant="h5" color="textSecondary">
+                    {result.title}
+                  </Typography>
+                  <Typography variant="h6">
+                    <time datetime={result.humanDate}>{result.humanDate}</time>
+                    {" "}&middot;{" "}
+                    {result.timeToRead} min read
+                  </Typography>
+                  {result.excerpt}
+                </CardContent>
+              </CardActionArea>
+            </Box>
+        ))}
+      </Fragment>
     </Layout>
   );
 };
