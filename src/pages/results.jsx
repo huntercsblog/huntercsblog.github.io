89dcbe26b6445043186e3dd18641d3495d52e7c7
Alicia <akthyang@gmail.com>
2021-07-28 20:36:35 -0400

    Added index and store into results.jsx

--- src/pages/results.jsx
+++ src/pages/results.jsx
@@ -2,8 +2,38 @@ import React, {useState} from "react";
 import Layout from "src/layout";
 import Typography from "@material-ui/core/Typography";
 import "src/assets/styles/results.css";
+// import { useFlexSearch } from 'react-use-flexsearch';
+import { graphql, useStaticQuery } from "gatsby";
 
 const Result = props => {
+  const pageQuery =useStaticQuery(graphql`
+    query {
+      localSearchPages {
+        index
+        store
+      }
+      allMdx(
+        sort: { fields: frontmatter___date, order: DESC }
+      ) {
+        edges {
+          node {
+            frontmatter {
+              title
+              humanDate: date(formatString: "MMM Do, YYYY")
+              datetime: date(formatString: "YYYY-MM-DD")
+            }
+            fields {
+              slug
+            }
+            excerpt(truncate: true, pruneLength: 300)
+            timeToRead
+            rawBody
+          }
+        }
+      }
+    }
+  `);
+  const index = pageQuery.index;
   return (
     <Layout>
       <div id = "Search">
@@ -11,6 +41,7 @@ const Result = props => {
           type = "text"
           placeholder = "Search posts"
           id = "inptxt"
+
         />
           <div class="icons8-search"></div>
       </div>
