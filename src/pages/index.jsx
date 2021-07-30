1b8ab259906b98b57db24fdd87738b7b1d427e54
TanmoyPaul1 <pault@bxscience.edu>
2021-07-30 17:50:08 -0400

    Redesigned the homepage layout
    Co-authored-by: hasiddiqui2 <hasiddiqui2@users.noreply.github.com>
    Co-authored-by: Vasyl Ilnytskyy <vilnytskyy@users.noreply.github.com>

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,7 +2,9 @@ import React from "react";
 import { graphql, useStaticQuery} from "gatsby";
 import Layout from "src/layout";
 import ArticleList from "../components/article-list";
+import HomePage from "../components/homepage";
 import SubscribeButton from "../components/SubscribeButton";
+import Grid from '@material-ui/core/Grid'
 
 // Index for homepage layout
 const Index = () => {
@@ -39,14 +41,25 @@ const Index = () => {
   const getTaggedArticles = (name) => {
     return recent.slice(3).filter(edge => edge.node.frontmatter.tags.some(tag => tag === name)).slice(0, 2);
   }
-
+  
   return (
     <Layout>
+      {/* 3 featured articles at the top */}
       <ArticleList articles={recent.slice(0, 3)} title="Latest News" />
-      <ArticleList articles={getTaggedArticles("Hunter")} title="Hunter" />
-      <ArticleList articles={getTaggedArticles("NYC")} title="NYC" />
-      <ArticleList articles={getTaggedArticles("Daedalus")} title="Daedalus" />
-      <ArticleList articles={getTaggedArticles("Tech")} title="Tech" />
+      <Grid container spacing={2}>
+        <Grid item xs={12} md={6}>
+            <ArticleList articles={getTaggedArticles("Hunter")} title="Hunter" />
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <ArticleList articles={getTaggedArticles("Tech")} title="Tech" />
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <ArticleList articles={getTaggedArticles("NYC")} title="NYC" />
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <ArticleList articles={getTaggedArticles("Daedalus")} title="Daedalus" />
+        </Grid>
+      </Grid>
       <SubscribeButton />
     </Layout>
   );
