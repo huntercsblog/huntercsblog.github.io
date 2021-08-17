50a5671ed78e3696d38f28c71267bb662a50a6bb
Alicia <akthyang@gmail.com>
2021-08-17 15:27:44 -0400

    Adjusted code for the pagination component

--- src/templates/index.jsx
+++ src/templates/index.jsx
@@ -1,46 +1,18 @@
 import React from "react";
-import { graphql, Link } from "gatsby";
+import { graphql } from "gatsby";
 import Layout from "src/layout";
 import ArticleList from "../components/article-list";
 import SubscribeButton from "../components/SubscribeButton";
-import Grid from "@material-ui/core/Grid";
-import ArrowBackIcon from "@material-ui/icons/ArrowBack";
-import ArrowForwardIcon from "@material-ui/icons/ArrowForward";
-import "../assets/styles/pagination.css";
+import Pagination from "../components/pagination.jsx";
 
 const IndexPage = ({ data, pageContext }) => {
   const recent = data.allMdx.edges;
-  const { currentPage, numPages } = pageContext;
-  const isFirst = currentPage === 1;
-  const isLast = currentPage === numPages;
-  const prevPage = currentPage - 1 === 1 ? "/" : `/${currentPage - 1}`;
-  const nextPage = `/${currentPage + 1}`;
+  const { numPages } = pageContext;
 
   return (
     <Layout>
       <ArticleList articles={recent} title="Latest News" />
-      <Grid container justify="space-between">
-        <Grid item>
-          {!isFirst && (
-            <Grid container alignItems="center">
-              <ArrowBackIcon />
-              <Link to={prevPage} rel="prev" id = "prev">
-                Previous Page
-              </Link>
-            </Grid>
-          )}
-        </Grid>
-        <Grid item>
-          {!isLast && (
-            <Grid container alignItems="center">
-            <Link to={nextPage} rel="next" id = "next">
-              Next Page
-            </Link>
-              <ArrowForwardIcon />
-            </Grid>
-          )}
-        </Grid>
-      </Grid>
+      <Pagination numPages = {numPages}/>
       <SubscribeButton />
     </Layout>
   );
